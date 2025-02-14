0421b06ab2726ee29142e9692fd3ab3d9ce01330
jbazan <jbazan@newpay.com.ar>
2025-02-14 12:51:57 -0300

    new test

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ function agregarHistorial(persona) {
   historial.push({ fecha: hoy, persona: persona.nombre }); // Guarda solo el nombre
   try {
     fs.writeFileSync(archivoHistorial, JSON.stringify(historial, null, 2));
+    historial = JSON.parse(fs.readFileSync(archivoHistorial));
   } catch (error) {
-    console.error("Error al escribir el archivo de historial:", error);
+    console.error("Error al escribir o leer el archivo de historial:", error);
   }
 }
 
@@ -64,10 +65,9 @@ app.get("/", (req, res) => {
 
 app.get("/saltar", (req, res) => {
   const actual = obtenerGuardiaActual();
-
   let index = personas.findIndex(p => p.nombre === actual);
   let siguiente = (index + 1) % personas.length;
-  agregarHistorial(personas[siguiente].nombre);
+  agregarHistorial(personas[siguiente]);
   res.json({ mensaje: `Guardia pasada a ${personas[siguiente].nombre}` });
 });
 
