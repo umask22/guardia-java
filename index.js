ce8675a0a5f29b67730694250a481c09c7e60993
jbazan <jbazan@newpay.com.ar>
2025-02-14 12:04:59 -0300

    express modify

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = required("express");
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("hola mundo");
